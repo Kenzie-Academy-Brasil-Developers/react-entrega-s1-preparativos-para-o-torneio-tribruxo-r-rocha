d6961d90e143b5e33f02263b40e8bb507c5c1b86
Roberto Rocha <rsantos.rocha@gmail.com>
2021-09-10 12:48:56 -0300

    Update: implementation of business rules

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
 
   const [students, setStudents] = useState([]);
 
+  const [players, setPlayers] = useState([])
+
   useEffect(() => {
     fetch("https://hp-api.herokuapp.com/api/characters/students")
       .then((response) => response.json())
@@ -13,10 +15,32 @@ function App() {
       .catch((err) => console.log(err))
   }, []);
 
+  const selectPlayers = () => {
+    /** Seleciona Jogador 1 */
+    const player1 = students[Math.floor(Math.random() * 10)];
+    console.log(students)
+    console.log(player1);
+
+    /** Seleciona Jogador 2 */
+    const players2 = students.filter((elem) => elem.house !== player1.house);
+    console.log(players2);
+    const player2 = players2[Math.floor(Math.random() * players2.length)];
+    console.log(player2);
+
+    /** Seleciona Jogador 3 */
+    const players3 = players2.filter((elem) => elem.house !== player2.house);
+    console.log(players3);
+    const player3 = players3[Math.floor(Math.random() * players2.length)];
+    console.log(player3);
+
+    setPlayers([player1, player2, player3]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        {students.map((elem, index) => <Student key={index} name={elem.name} house={elem.house} alive={elem.alive} yearOfBirth={elem.yearOfBirth} image={elem.image} />)}
+        {/** students.map((elem, index) => <Student key={index} name={elem.name} house={elem.house} alive={elem.alive} yearOfBirth={elem.yearOfBirth} image={elem.image} />) */}
+        <button onClick={() => selectPlayers()}>Selecionar Jogadores</button>
       </header>
     </div>
   );
--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -1,14 +1,35 @@
 import "./style.css";
 
 const Student = ({ name, house, alive, yearOfBirth, image }) => {
-    return (
-        <div className="card">
-            <img src={image} alt={name} />
-            <div>{name}</div>
-            <div>{house}</div>
-            <div>{yearOfBirth}</div>
-        </div>
-    )
-}
+  return house === "Gryffindor" ? (
+    <div className="Card Gryffindor">
+      <img src={image} alt={name} />
+      <div>{name}</div>
+      <div>{house}</div>
+      <div>{yearOfBirth}</div>
+    </div>
+  ) : house === "Slytherin" ? (
+    <div className="Card Slytherin">
+      <img src={image} alt={name} />
+      <div>{name}</div>
+      <div>{house}</div>
+      <div>{yearOfBirth}</div>
+    </div>
+  ) : house === "Ravenclaw" ? (
+    <div className="Card Ravenclaw">
+      <img src={image} alt={name} />
+      <div>{name}</div>
+      <div>{house}</div>
+      <div>{yearOfBirth}</div>
+    </div>
+  ) : (
+    <div className="Card Hufflepuff">
+      <img src={image} alt={name} />
+      <div>{name}</div>
+      <div>{house}</div>
+      <div>{yearOfBirth}</div>
+    </div>
+  );
+};
 
 export default Student;
